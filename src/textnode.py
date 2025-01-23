faf09bdd809c2b2835c2767469d54cf21fecb918
Jochem Kuipers <jochem@kuipers.cc>
2025-01-23 08:16:22 +0100

    Refactor text_node_to_html_node method to use match-case syntax for improved readability and maintainability

--- src/textnode.py
+++ src/textnode.py
@@ -22,16 +22,18 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
     def text_node_to_html_node(self, text_node):
-        if text_node.text_type == TextType.TEXT:
-            return LeafNode(None, text_node.text)
-        if text_node.text_type == TextType.BOLD:
-            return LeafNode("b", text_node.text)
-        if text_node.text_type == TextType.ITALIC:
-            return LeafNode("i", text_node.text)
-        if text_node.text_type == TextType.CODE:
-            return LeafNode("code", text_node.text)
-        if text_node.text_type == TextType.LINKS:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        if text_node.text_type == TextType.IMAGES:
-            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
-            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(text_node.text)
+            case TextType.BOLD:
+                return LeafNode(f"<b>{text_node.text}</b>")
+            case TextType.ITALIC:
+                return LeafNode(f"<i>{text_node.text}</i>")
+            case TextType.CODE:
+                return LeafNode(f"<code>{text_node.text}</code>")
+            case TextType.LINKS:
+                return LeafNode(f'<a href="{text_node.url}">{text_node.text}</a>')
+            case TextType.IMAGES:
+                return LeafNode(f'<img src="{text_node.url}" alt="{text_node.text}">')
+            case _:
+                raise ValueError("Invalid TextType")
